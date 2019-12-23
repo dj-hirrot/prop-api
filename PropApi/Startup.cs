6b79465ddad8bcdceebda23ccbcf91e15381cd3c
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-23 17:13:46 +0900

    update: DIの登録

--- PropApi/Startup.cs
+++ PropApi/Startup.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PropApi.Models;
 
 namespace PropApi
 {
@@ -18,6 +20,7 @@ namespace PropApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
