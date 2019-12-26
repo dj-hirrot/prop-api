f2dc46d9719696bbeef6c3db4c429e2d7776cf74
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-26 12:41:51 +0900

    update: destroyメソッドの追加

--- PropApi/Controllers/TodoController.cs
+++ PropApi/Controllers/TodoController.cs
@@ -67,5 +67,22 @@ namespace PropApi.Controllers
 
             return NoContent();
         }
+
+        // DELETE: api/todo/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
