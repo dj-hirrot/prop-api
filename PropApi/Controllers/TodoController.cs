c7b0fe46eabb56e7bb17af23a43b9baaeefd1462
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-26 11:30:47 +0900

    add: TodoControllerの作成

---
+++ PropApi/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PropApi.Models;
+
+namespace PropApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
